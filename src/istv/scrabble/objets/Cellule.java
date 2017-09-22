1a54a0e6497ab092110001e208b69b7db20c0408
Sadgasmification <jourdain.nicolas1997@gmail.com>
2017-09-22 11:10:14 +0200

    Ajout du target/ dans le .gitignore

--- src/istv/scrabble/objets/Cellule.java
+++ src/istv/scrabble/objets/Cellule.java
@@ -7,10 +7,11 @@ public class Cellule {
     
     //Constructeur
     public Cellule(){
-        i= 100;
-        j= 200;
+        i= -1;
+        j= -1;
         estVide= true;
     }
+    
     public void genererCelluleVide(){
         Cellule cellule = new Cellule();
     }
---
+++ src/istv/scrabble/objets/MainJoueur.java
@@ -0,0 +1,23 @@
+/**
+ * 22-09-2017
+ * @author Jourdain Nicolas
+ * Licence 3 FA - ISTV
+ * 
+ */
+package istv.scrabble.objets;
+
+/**
+ *  
+ * MainJoueur.java
+ *
+ */
+public class MainJoueur {
+
+	/**
+	 * Constructeur de MainJoueur
+	 */
+	public MainJoueur() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
