cd189618d312cba204efdb76d4f5e21a20baea0e
aminebms <amineboumoussou@enaco.fr>
2017-09-22 10:42:40 +0200

    add Cellule class

--- src/istv/scrabble/objets/Cellule.java
+++ src/istv/scrabble/objets/Cellule.java
@@ -1,5 +1,17 @@
 package istv.scrabble.objets;
 
 public class Cellule {
-
+    int i;
+    int j;
+    Boolean estVide;
+    
+    //Constructeur
+    public Cellule(){
+        i= 100;
+        j= 200;
+        estVide= true;
+    }
+    public void genererCelluleVide(){
+        Cellule cellule = new Cellule();
+    }
 }
