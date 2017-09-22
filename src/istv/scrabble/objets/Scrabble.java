4f6c8e1e0768c0201328bce54af11dd7a11ea787
LEFEVRE Romain <lefmain@hotmail.fr>
2017-09-22 11:12:02 +0200

    ajout Class Scrabble

---
+++ src/istv/scrabble/objets/Scrabble.java
@@ -0,0 +1,49 @@
+package istv.scrabble.objets;
+
+import java.util.List;
+import java.util.TreeSet;
+
+public class Scrabble {
+	Plateau plateau;
+	List<Character> pioche;
+	TreeSet<String> dico;
+	
+	public void piocheLettre(){
+		
+	}
+	
+	public void verifMotsTree(){
+		
+	}
+	
+	public void ajoutMotsTree(){
+		
+	}
+
+	public Plateau getPlateau() {
+		return plateau;
+	}
+
+	public void setPlateau(Plateau plateau) {
+		this.plateau = plateau;
+	}
+
+	public List<Character> getPioche() {
+		return pioche;
+	}
+
+	public void setPioche(List<Character> pioche) {
+		this.pioche = pioche;
+	}
+
+	public TreeSet<String> getDico() {
+		return dico;
+	}
+
+	public void setDico(TreeSet<String> dico) {
+		this.dico = dico;
+	}
+	
+	
+
+}
