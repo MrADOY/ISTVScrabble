0cd84e3ca7b7e29c12cfdcf793ea2d521bfd0560
LEFEVRE Romain <lefmain@hotmail.fr>
2017-09-28 16:20:24 +0200

    Ajout fonction verifMots et utilisation du TreeSet

--- src/istv/scrabble/objets/Scrabble.java
+++ src/istv/scrabble/objets/Scrabble.java
@@ -7,20 +7,41 @@
 
 package istv.scrabble.objets;
 
+import istv.scrabble.interfaces.Plateau;
+import java.io.*;
 import java.util.List;
 import java.util.TreeSet;
 
 public class Scrabble {
 	Plateau plateau;
 	List<Character> pioche;
-	TreeSet<String> dico;
+
+	//DECLARATION DES VARIABLES / TREESET
+	String monMot;
+	String monFichier = "./src/dico.txt";
+	TreeSet<String> set = new TreeSet<String>();
 	
 	public void piocheLettre(){
 		
 	}
-	
-	public void verifMotsTree(){
-		
+
+	// NAVIGATION DANS MON FICHIER TXT ET AJOUT DES LIGNES DANS TREESET
+	public void verifMotsTree(String monMot){
+		try{
+			InputStream ips = new FileInputStream(monFichier);
+			InputStreamReader ipsr = new InputStreamReader(ips);
+			BufferedReader br = new BufferedReader(ipsr);
+			String ligne;
+			while((ligne=br.readLine()) != null){
+				set.add(ligne);
+			}
+			br.close();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		this.monMot = monMot;
 	}
 	
 	public void ajoutMotsTree(){
@@ -44,13 +65,20 @@ public class Scrabble {
 	}
 
 	public TreeSet<String> getDico() {
-		return dico;
+		return set;
 	}
 
 	public void setDico(TreeSet<String> dico) {
-		this.dico = dico;
+		this.set = dico;
 	}
-	
-	
+
+	// METHODE TOSTRING QUI RENVOIE SI OUI OU NON LA VALEUR EXISTE DANS LA LISTE TREESET
+	public String toString() {
+		if(set.contains(monMot.toUpperCase()) == true){
+			return "Le mot " + monMot.toLowerCase() + " existe dans la liste !";
+		}else{
+			return "Le mot " + monMot.toLowerCase() + " n'existe pas dans la liste !";
+		}
+	}
 
 }
