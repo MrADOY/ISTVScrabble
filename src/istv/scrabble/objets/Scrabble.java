c7d17e04f1f0ed20c71506e552c8a3ae75b018c2
Aurelien <pietrzak.aurelien@gmail.com>
2017-10-27 10:07:40 +0200

    [Scrabble] add JoueurActuel

--- src/istv/scrabble/objets/Scrabble.java
+++ src/istv/scrabble/objets/Scrabble.java
@@ -12,13 +12,34 @@ public class Scrabble {
 
 	protected PlateauImpl plateau;
 	protected Pioche pioche;
-	protected Joueur[] joueurs;
+	protected static Joueur[] joueurs;
+	protected static Joueur joueurActuel;
+	
+	
 	
 	public Scrabble() {
 		plateau = new PlateauImpl();
 		pioche = new Pioche();
 		joueurs = new Joueur[2];
 		
+	}
+	
+ 
+	public static Joueur getJoueurActuel() {
+		return joueurActuel;
+	}
+	
+	public static void setJoueurActuel(Joueur j) {
+		Scrabble.joueurActuel = j;
+	}
+	
+	public static void switchJoueur() {
+		if (Scrabble.joueurActuel.equals(joueurs[0])) {
+			Scrabble.setJoueurActuel(joueurs[1]);
+		}
+		else {
+			Scrabble.setJoueurActuel(joueurs[0]);
+		}
 	}
 	
 }
